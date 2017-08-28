2d47d063a12fa3c0dff0aa92e2528e3e1328786a
CeceSoudaly <ccsoudal@yahoo.com>
2017-08-27 21:29:48 -0500

    adding more data files

---
+++ model/Location+CoreDataProperties.swift
@@ -0,0 +1,24 @@
+//
+//  Location+CoreDataProperties.swift
+//  VirtualMapTour
+//
+//  Created by Cece Soudaly on 8/27/17.
+//  Copyright © 2017 CeceMobile. All rights reserved.
+//
+
+import Foundation
+import CoreData
+
+
+extension Location {
+
+    @nonobjc public class func fetchRequest() -> NSFetchRequest<Location> {
+        return NSFetchRequest<Location>(entityName: "Location")
+    }
+
+    @NSManaged public var latitude: Double
+    @NSManaged public var longtitude: Double
+    @NSManaged public var name: String?
+    @NSManaged public var photo: Photo?
+
+}
